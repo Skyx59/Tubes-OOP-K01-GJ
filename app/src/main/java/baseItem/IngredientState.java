4af69a760c680b1360677c80d42b942d7471620e
18224035-dotcom <18224035@std.stei.itb.ac.id>
2025-12-09 19:18:40 +0700

    Add IngredientState enum for ingredient states

---
+++ app/src/main/java/baseItem/IngredientState.java
@@ -0,0 +1,10 @@
+package BaseItem;
+
+public enum IngredientState {
+    RAW,//mentah
+    CHOPPED,//terpotong (untuk lettuce atau tomat)
+    COOKING, //sedang dimasak
+    COOKED, //patty siap saji
+    BURNED //patty kelamaan dimasak
+    
+}
