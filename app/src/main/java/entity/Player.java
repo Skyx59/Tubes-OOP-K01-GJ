74d25067be348a56acc14382c698819aefea1f84
Rezky <rezky.muhammad.hafiz80@gmail.com>
2025-12-09 18:43:27 +0700

    feat: add Entity and Player for the ChefPlayer in the game and in the GUI

--- app/src/main/java/entity/Player.java
+++ app/src/main/java/entity/Player.java
@@ -2,6 +2,11 @@ package entity;
 
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import java.awt.Color;
 
 import view.*;
@@ -10,10 +15,12 @@ import controller.*;
 public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH; 
+    String color;
 
-    public Player(GamePanel gp, KeyHandler keyH){
+    public Player(GamePanel gp, KeyHandler keyH, String color){
         this.gp = gp;
         this.keyH = keyH;
+        this.color = color;
 
         solidArea = new Rectangle();
         solidArea.x = 8;
@@ -22,6 +29,7 @@ public class Player extends Entity{
         solidArea.height = 32;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues(){
@@ -29,6 +37,25 @@ public class Player extends Entity{
         x = 290;
         y = 95;
         speed = 1;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+
+        try{
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/"+ color + "_right_2.png"));
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update(){
@@ -57,6 +84,17 @@ public class Player extends Entity{
                 goalX = x + gp.tileSize; // Target x adalah 1 tile ke kanan
                 goalY = y;
             }
+
+            spriteCounter++;
+            if(spriteCounter > 200){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
         
         // === Bagian 2: Melakukan Pergerakan ke Tujuan ===
@@ -67,6 +105,7 @@ public class Player extends Entity{
             gp.cChecker.checkTile(this); // Asumsi checkTile() diperbarui untuk mengecek goalX/goalY
             // CATATAN: Karena checkTile yang sebelumnya Anda miliki hanya melihat KeyHandler, 
             // Anda perlu memastikan CollisionChecker membandingkan goalX/goalY dengan tile.
+            gp.cChecker.checkPlayer(this, gp.players, gp.acivePlayerIndex);
             
             // Jika TIDAK ada tabrakan, lakukan pergerakan
             if (collisionOn == false) {
@@ -78,7 +117,7 @@ public class Player extends Entity{
                         if (y <= goalY) { // Cek jika sudah mencapai atau melewati target
                             y = goalY;
                             isMoving = false; // Berhenti bergerak
-                            direction = "stay";
+                            direction = "up";
                         }
                         break;
                     case "down":
@@ -86,7 +125,7 @@ public class Player extends Entity{
                         if (y >= goalY) {
                             y = goalY;
                             isMoving = false;
-                            direction = "stay";
+                            direction = "down";
                         }
                         break;
                     case "left":
@@ -94,7 +133,7 @@ public class Player extends Entity{
                         if (x <= goalX) {
                             x = goalX;
                             isMoving = false;
-                            direction = "stay";
+                            direction = "left";
                         }
                         break;
                     case "right":
@@ -102,21 +141,73 @@ public class Player extends Entity{
                         if (x >= goalX) {
                             x = goalX;
                             isMoving = false;
-                            direction = "stay";
+                            direction = "right";
                         }
                         break;
                 }
             } else {
                 // Jika ada tabrakan, batalkan pergerakan dan reset status
                 isMoving = false; 
-                direction = "stay";
+                switch (direction) {
+                    case "up":
+                        direction = "up";
+                        break;
+                    case "down":
+                        direction = "down";
+                        break;
+                    case "left":
+                        direction = "left";
+                        break;
+                    case "right":
+                        direction = "right";
+                        break;
                 // TIDAK perlu mengubah x/y karena tabrakan mencegah inisiasi pergerakan
+                }
             }
         }
     }
 
     public void draw(Graphics2D g2){
-        g2.setColor(Color.black);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        //g2.setColor(Color.black);
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                if(spriteNum == 1){
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
